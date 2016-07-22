5cf1529892ece520ea661c818191a31bfffc8883
emotiona_xiaoshi <emotiona_xiaoshi@126.com>
2016-07-22 14:13:04 +0800

    1、测试添加登录界面Activity

---
+++ LiliLalaProject/app/src/main/java/com/android/lala/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.android.lala.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.lala.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
